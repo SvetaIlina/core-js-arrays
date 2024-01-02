e88039b87f0a12017094166891e2b9ee0276f53e
Svetlana <svetailina1987@gmail.com>
2024-01-02 18:11:24 +0400

    feat:add isSameLength solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -135,8 +135,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const elLength = arr[0].length;
+  return arr.every((i) => i.length === elLength);
 }
 
 /**
