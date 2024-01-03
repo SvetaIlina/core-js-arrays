389936996f7264e8203746e888135444ec2a9f56
Svetlana <svetailina1987@gmail.com>
2024-01-03 16:27:50 +0400

    feat: add sumArrays solutiion

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const max = Math.max(arr1.length, arr2.length);
+  const sumArr = Array.from({ length: max }, (v, k) => {
+    if (arr1[k] === undefined) return arr2[k];
+    if (arr2[k] === undefined) return arr1[k];
+    return arr1[k] + arr2[k];
+  });
+  return sumArr;
 }
 
 /**
