0d75f3e6023aed2ef63f48a2834a811ecab4248e
Svetlana <svetailina1987@gmail.com>
2024-01-03 16:58:22 +0400

    feat: add flattenArray solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -287,8 +287,11 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.reduce(
+    (acc, item) => acc.concat(Array.isArray(item) ? flattenArray(item) : item),
+    []
+  );
 }
 
 /**
